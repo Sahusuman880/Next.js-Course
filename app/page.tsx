20ca9e26ebbb3603ed205c1492eafe0645fb217d
suman sahu <sahusuman880@gmail.com>
2026-05-18 21:27:41 +0530

    Learning Data Fetching in Nest.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,50 @@
-import Image from 'next/image'
-import React from 'react'
-import Counter from './components/Counter'
+// import Image from 'next/image'
+// import React from 'react'
+// import Counter from './components/Counter'
 
-async function Home() {
-  const data = await fetch('https://jsonplaceholder.typicode.com/posts').then(res=>res.json())
+// async function Home() {
+//   const data = await fetch('https://jsonplaceholder.typicode.com/posts').then(res=>res.json())
+//   return (
+//     <main style={{padding:"40px", textAlign:"center", fontWeight:700}}>
+//         <h1>Home Page</h1>
+//         <p>This is Main Component</p>
+//         <Image 
+//         // src="/hero.jpg"
+//         src="https://images.unsplash.com/photo-1702138129392-364adea0ad00?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c3VwZXJtYW58ZW58MHx8MHx8fDA%3D"
+//         alt="Hero Image"
+
+//         width={100}
+//         height={100}/>
+//         <ul>
+//         {data.map((item:{id:number, title:string, body:string})=>(
+//           <li key={item.id}>
+//             <h2 style={{color:'blue'}}>{item.title}</h2>
+//             <p style={{color:'blue'}}>{item.body}</p>
+//           </li>
+//         ))}
+//         </ul>
+//         <Counter/>
+//     </main>
+//   )
+// }
+
+// export default Home
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+export default async function Home() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts",{
+    cache:'no-store'
+  });
+  const data: Post[] = await response.json();
   return (
     <main style={{padding:"40px", textAlign:"center", fontWeight:700}}>
         <h1>Home Page</h1>
         <p>This is Main Component</p>
-        <Image 
-        // src="/hero.jpg"
-        src="https://images.unsplash.com/photo-1702138129392-364adea0ad00?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c3VwZXJtYW58ZW58MHx8MHx8fDA%3D"
-        alt="Hero Image"
-
-        width={100}
-        height={100}/>
         <ul>
         {data.map((item:{id:number, title:string, body:string})=>(
           <li key={item.id}>
@@ -23,9 +53,6 @@ async function Home() {
           </li>
         ))}
         </ul>
-        <Counter/>
     </main>
   )
 }
-
-export default Home
